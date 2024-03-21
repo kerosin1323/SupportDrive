2c85a09d767ad16f38ebb5da4255da453e845476
ermolenkokirill <ermolenkokirill132@icloud.com>
2024-03-21 16:30:18 +0300

    Create Test v2

--- forms/TestForm.py
+++ forms/TestForm.py
@@ -7,6 +7,14 @@ class MakingTestForm(FlaskForm):
     name = StringField('Название', validators=[DataRequired()])
     category = StringField('Категория', validators=[DataRequired()])
     describe = StringField('Описание')
-    photo = FileField('Фото')
+    photo = FileField('Фото', validators=[DataRequired()])
     add_question = SubmitField('+ Добавить вопрос +')
     create = SubmitField('Создать')
+
+
+class MakingQuestion(FlaskForm):
+    photo = FileField('Фото (600x600)')
+    question = StringField('Текст вопроса')
+    describe = StringField('Описание (Необязательно)')
+    add_answer = SubmitField('+ Добавить вариант ответа +')
+
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
-from data import db_session
-from forms.make_test_form import MakingTestForm
+from data import db_session, tests, users
+from forms.TestForm import MakingTestForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -9,6 +9,11 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 @app.route('/make_test', methods=['GET', 'POST'])
 def make_test():
     form = MakingTestForm()
+    if form.validate_on_submit():
+        test = tests.Tests()
+        test.name = form.name.data
+        test.about = form.describe.data
+        test.photo = form.photo.data
     return render_template('make_test.html', title='Создание теста', form=form)
 
 
