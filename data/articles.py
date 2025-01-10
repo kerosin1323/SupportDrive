931749e18ed738ef911cb4d52faff4f1a245f9fd
vandamq <kuskovi09@mail.ru>
2025-01-10 18:45:47 +0300

    main page + profile

--- data/articles.py
+++ data/articles.py
@@ -1,4 +1,6 @@
 import datetime
+from email.policy import default
+
 import sqlalchemy
 from sqlalchemy import orm
 from data.db_session import SqlAlchemyBase
@@ -11,11 +13,12 @@ class Articles(SqlAlchemyBase):
     name = sqlalchemy.Column(sqlalchemy.String)
     key_words = sqlalchemy.Column(sqlalchemy.String)
     categories = sqlalchemy.Column(sqlalchemy.String)
-    created_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
+    describe = sqlalchemy.Column(sqlalchemy.String)
+    created_date = sqlalchemy.Column(sqlalchemy.String, default=datetime.datetime.now)
     text = sqlalchemy.Column(sqlalchemy.String, nullable=False)
     mark = sqlalchemy.Column(sqlalchemy.Integer, default=0)
     photo = sqlalchemy.Column(sqlalchemy.String)
-    readings = sqlalchemy.Column(sqlalchemy.Integer)
+    readings = sqlalchemy.Column(sqlalchemy.Integer, default=0)
     user_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('users.id'))
     user = orm.relationship('User')
 
